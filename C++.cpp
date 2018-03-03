cbb157953d76fe61bd550651dd324ea334d8b615
LSR1999 <36955987+LSR1999@users.noreply.github.com>
2018-03-03 18:00:35 +0800

    Update C++.cpp
    to draw a rhombus formed with "*"

--- C++.cpp
+++ C++.cpp
@@ -1,1 +1,25 @@
-# include
+# include<iostream>
+using namespace std;
+
+int main()
+{
+	int i, j, k;
+	for (i = 0; i <= 2; i++)
+	{
+		for (k = 2; k > i; k--)
+			cout<<" ";
+			for (j = 1; j <= 2 * i + 1; j++)
+				cout<<"*";
+			cout<<"\n";
+	}
+	for (i = 3; i <= 4; i++)
+	{
+		for (k = 1; k <= i - 2; k++)
+			cout<<" ";
+		for (j = 1; j <= 9 - 2 * i; j++)
+			cout<<"*";
+		cout<<"\n";
+	}
+	system("pause");
+	return 0;
+}
